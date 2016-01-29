12b727a8a582619de938e818659677b8581ed58f
Freddy <madeup@email.com>
2016-01-29 00:53:03 +0000

    First commit.

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChessLib;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessPosition pos = new ChessPosition();
+            string fenStr = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+            int total, leaves, castles;
+            for (int d = 1; d <= 5; d++)
+            {
+            }
+            Console.ReadLine();
+        }
+    }
+}
